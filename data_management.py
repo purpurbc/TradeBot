a7016e5f22a2b659e20d448ee66632421c6e6133
Anton <karlantonjaderblom@gmail.com>
2022-09-05 15:37:12 +0200

    mvd provisional plot functions to a separate file

--- data_management.py
+++ data_management.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -100,47 +98,7 @@ def log_and_save_data(data_values,time_values,file_name,df,wanted_data,index):
     #append the time to the time_values list
     time_values.append((datetime.now(tz=timezone.utc)+timedelta(hours=2)).timestamp())
 
-"""annotate either max or min value of specified data_values"""
-def annot(y,x,type):
-
-    xval = 0
-    yval = 0
-
-    #get max/min values
-    if type == 'max':
-        xval = x[np.argmax(y)]
-        yval = max(y)
-    elif type == 'min':
-        xval = x[np.argmin(y)]
-        yval = min(y)
-
-    #create text
-    text= "{:.3f}".format(float(yval))
-
-    ax=plt.gca()
-
-    #set the text at xy-pos with a specific style
-    style = dict(size=10, color='gray')
-    ax.text(xval, yval, text, **style)
-
-"""set the spacing for the xticks in the plot"""
-def set_plt_xticks(len_time_values,step_size):
-    #regulate the spacing between the xlabels 
-    step_size = (len_time_values/40)*5
-    if step_size < 5: step_size = 5
-    plt.xticks(np.arange(start=0, stop=len_time_values, step=step_size))
-
-"""plot the graph for the data_values"""
-def plot_graph(data_values,time_values,crypto_name,line_color):
-
-    marker_size = 4 #size of the marker when plotting
-
-    if len(time_values) > 80: 
-        marker_size = 0
-
-    #plot,legend,pause and clear
-    plt.plot([(datetime.fromtimestamp(x)).strftime("%m/%d/%Y\n%H:%M:%S") for x in time_values], data_values, linestyle='solid', marker='p', ms=marker_size,  color=line_color, label=crypto_name)
-    
+ 
 """open the save_file and read its contents. Store it as a pair of lists and return it"""    
 def get_saved_data(file_name):
     
@@ -168,7 +126,7 @@ def get_saved_data(file_name):
 
 
 """Reads a .csv file and plots the data with pyplot"""
-def get_csv_data(filename, columnname, col_list, length, plot):
+def get_csv_data(filename, columnname, col_list, length):
     
     #read csv file
     df = pd.read_csv(filename, usecols=col_list)
@@ -183,11 +141,6 @@ def get_csv_data(filename, columnname, col_list, length, plot):
     time_stamps = list(map(float,df["unix"][0:length]))
     #time_stamps = [x - start_time for x in time_stamps]
 
-    #plot a graph of the gathered data
-    if plot:
-        plt.plot(time_stamps, data)
-        plt.show()
-    
     #return the data-list
     return data
 
